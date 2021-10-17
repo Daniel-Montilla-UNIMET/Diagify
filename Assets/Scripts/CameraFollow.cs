1b1998c44d8f5930eea668b1104f284039fe3e7a
Daniel Montilla <montilla.d2000@gmail.com>
2021-10-17 13:42:54 -0400

    camera sigue al jugador

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, -10);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = new Vector3(target.position.x, target.position.y, -10);
+    }
+}
