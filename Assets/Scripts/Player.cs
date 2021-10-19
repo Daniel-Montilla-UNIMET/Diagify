12268c4ddf4ccaa1760be6f0663d8a411a8d84e1
Titranx <90871351+Titranx@users.noreply.github.com>
2021-10-18 22:31:13 -0400

    "Se agrego Las animaciones y el texto"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     public float speed = 32f;
     public Joystick joystick;
     public Rigidbody2D body;
+    public Animator animator;
+    
 
     float dx = 0, dy = 0;
 
@@ -14,5 +16,13 @@ public class Player : MonoBehaviour
         dx = joystick.Horizontal() * speed;
         dy = joystick.Vertical() * speed;
         body.velocity = new Vector2(dx, dy);
+
+        if (dx == 0 && dy == 0){
+            animator.SetFloat("Speed",0);
+        }else{
+            animator.SetFloat("Speed",1f);
+        }
+
+
     }
 }
