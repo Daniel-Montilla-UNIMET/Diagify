7109903027a79b8fe7b698badc725bc267379b25
xapho <kevin.han@correo.unimet.edu.ve>
2021-11-20 22:30:27 -0400

    script slider

---
+++ Assets/Scripts/MenuVolumen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuVolumen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider slider;
+    public float sliderValue;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("volumenAudio",0.5f;
+        AudioListener.volume = slider.value;
+        
+    }
+
+    public void ChangeVolume(float valor){
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("volumenAudio",sliderValue);
+        AudioListener.volume = slider.value;
+    }
+
+
+}
